e8a70d63135e02f873815cbed9d2da4e5be27c40
tejes <tmank14319@gmail.com>
2023-08-20 18:33:30 +0530

    basic profile section with dummy profiles

--- src/components/homescreen/HomeScreenMidSection.tsx
+++ src/components/homescreen/HomeScreenMidSection.tsx
@@ -1,6 +1,20 @@
+import { useRouter } from "next/router";
 import GenricReaButton from "./GenricButtonRed";
+import { useSession } from "next-auth/react";
 
 export default function HomeScreenMidSection() {
+  const router = useRouter()
+  const { data, status } = useSession()
+  const handleGetStarted = () => {
+    if (status == "authenticated") {
+      void router.push("/profiles")
+
+    }
+    if (status == "unauthenticated") {
+      void router.push("/browse")
+    }
+
+  }
   return (
     <div className="relative flex h-full w-full flex-col flex-wrap content-center justify-center text-center  ">
       <h1 className=" text-[2rem] font-bold 2xl:text-[3rem]">
@@ -11,9 +25,12 @@ export default function HomeScreenMidSection() {
         Read to watch? Create a free account to enjoy full experience
       </h3>
       <div className=" mt-5  flex w-full flex-wrap justify-center">
-        <div className="mt-5 h-[50px] w-[140px] 2xl:w-[170px]">
-          <GenricReaButton text={"Start Watching"} func={() => ""} />
-        </div>
+        {status == "loading" ? null :
+          <div className="mt-5 h-[50px] w-[140px] 2xl:w-[170px]">
+            <GenricReaButton text={"Start Watching"} func={handleGetStarted} />
+          </div>
+
+        }
       </div>
     </div>
   );
